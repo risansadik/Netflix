bb852a6d96387fb357b1aa642b43be0982bc419c
Muhammed Risan Bin Sadik <Risan Bin Sadik@Binsadik.BinSadique>
2025-04-19 09:16:00 +0530

    home page completed

---
+++ Netflix-clone/src/components/TitleCards/TitleCards.tsx
@@ -0,0 +1,44 @@
+import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data';
+import { useEffect, useRef } from 'react';
+
+const TitleCards = ({title,category}) => {
+  const cardsRef = useRef<HTMLDivElement>(null);
+
+  const handleWheel = (event: WheelEvent) => {
+    event.preventDefault();
+
+     if (cardsRef.current) {
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
+  };
+
+  useEffect(() => {
+    const currentRef = cardsRef.current;
+    if (currentRef) {
+      currentRef.addEventListener('wheel', handleWheel, { passive: false });
+    }
+
+    return () => {
+      if (currentRef) {
+        currentRef.removeEventListener('wheel', handleWheel);
+      }
+    };
+  }, []);
+
+  return (
+    <div className='title-cards'>
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
